a2d3c7633d299fca5e0f8cc4b9b24f92a27d71e6
jesseb34r <aubin.1+github@gmail.com>
2022-01-29 22:00:41 -0700

    added gameid page

---
+++ pages/game/[gameid].tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import type { NextPage } from "next";
+import { Box } from "@mui/material";
+
+const Game: NextPage = () => {
+  return <Box />;
+};
+
+export default Game;
